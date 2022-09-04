31b4b7ff9e2956500bf2d9e3527912678d6d8532
OliverAndreasen <oliv2058@stud.kea.dk>
2022-09-04 21:52:44 +0200

    init

---
+++ src/main/java/dat3/cars/api/CarController.java
@@ -0,0 +1,57 @@
+package dat3.cars.api;
+
+import dat3.cars.dto.CarRequest;
+import dat3.cars.dto.CarResponse;
+import dat3.cars.dto.MemberResponse;
+import dat3.cars.entity.Car;
+import dat3.cars.repository.CarRepository;
+import dat3.cars.service.CarService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/cars")
+public class CarController {
+
+    CarService carService;
+
+    public CarController(CarService carService){
+        this.carService = carService;
+    }
+
+    @GetMapping
+    public List<CarResponse> getCars(){
+        return carService.getCars();
+    }
+
+    @GetMapping(path = "/{id}")
+    public CarResponse getCarById(@PathVariable int id) throws Exception { //Obviously we need to be able to limit this in a system with thousands of members
+        CarResponse response = carService.findCarById(id);
+        return response;
+    }
+
+    @DeleteMapping("{id}")
+        public void deleteCarById(@PathVariable int id){
+        carService.deleteById(id);
+    }
+
+    @PatchMapping("Model/{id}/{value}")
+    public void setModelForCar(@PathVariable int id, @PathVariable String value){
+
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Boolean> editCar(@RequestBody CarRequest body, @PathVariable int id){
+        carService.editCar(body,id);
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/dat3/cars/dto/CarRequest.java
@@ -0,0 +1,34 @@
+package dat3.cars.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import dat3.cars.entity.Car;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CarRequest {
+
+    String brand;
+    String model;
+    double pricePrDay;
+    double bestDiscount;
+
+    public static Car getCarEntity(CarRequest c){
+        return new Car(c.getBrand(), c.getModel(), c.getPricePrDay(), c.getBestDiscount());
+    }
+
+    public CarRequest(Car c){
+        this.brand = c.getBrand();
+        this.model = c.getModel();
+        this.pricePrDay = c.getPricePrDay();
+        this.bestDiscount = c.getBestDiscount();
+    }
+
+}
